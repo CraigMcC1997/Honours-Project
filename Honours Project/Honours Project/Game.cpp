61cbb6505cab44bb69c1ac4b0c6c243fa78bd7ae
CraigMcCorrisken <craigmccorrisken@gmail.com>
2020-01-03 17:02:36 +0000

    0.0.7 - unable to load shaders
    Signed-off-by: CraigMcCorrisken <craigmccorrisken@gmail.com>

--- Honours Project/Honours Project/Game.cpp
+++ Honours Project/Honours Project/Game.cpp
@@ -2,6 +2,33 @@
 
 void Game::init()
 {
+	light0 = {
+	{ 0.2f, 0.2f, 0.2f, 1.0f }, // ambient
+	{ 0.7f, 0.7f, 0.7f, 1.0f }, // diffuse
+	{ 0.8f, 0.8f, 0.8f, 1.0f }, // specular
+	{ 0.0f, 0.0f, 1.0f, 1.0f } // position
+	};
+
+	material0 = {
+		{ 0.4f, 0.2f, 0.2f, 1.0f }, // ambient
+		{ 0.8f, 0.5f, 0.5f, 1.0f }, // diffuse
+		{ 1.0f, 0.8f, 0.8f, 1.0f }, // specular
+		2.0f // shininess
+	};
+
+	textures[0] = loadTexture::loadTextures("../Resources/Textures/fabric.bmp");
+
+	shaderProgram = rt3d::initShaders("../Resources/Shaders/phong.vert", "../Resources/Shaders/phong.frag");	//initialising the chosen shaders
+	/*rt3d::setLight(shaderProgram, light0);
+	rt3d::setMaterial(shaderProgram, material0);*/
+
+	//model loading
+	rt3d::loadObj("../Resources/3D_Objects/cube.obj", verts, norms, tex_coords, indices);
+	meshIndexCount[0] = indices.size();
+	meshObjects[1] = rt3d::createMesh(verts.size() / 3, verts.data(), nullptr, norms.data(), tex_coords.data(),
+		meshIndexCount[0], indices.data());
+	verts.clear(), norms.clear(), tex_coords.clear(), indices.clear();
+
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
@@ -18,7 +45,31 @@ void Game::draw(SDL_Window* window)
 	glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+	glm::mat4 modelview(1.0); // set base position for scene
+	glm::mat4 projection(1.0);
+	projection = glm::perspective(float(60.0f * DEG_TO_RADIAN), 800.0f / 600.0f, 1.0f, 150.0f);
+	rt3d::setUniformMatrix4fv(shaderProgram, "projection", glm::value_ptr(projection));
+	stack<glm::mat4> mvStack;
+	mvStack.push(modelview);
+
+	glm::mat4 tmp = mvStack.top();
+	rt3d::setLightPos(shaderProgram, glm::value_ptr(tmp));
+
+	//camera set up
+	camera::setAt(Move::moveForward(camera::getEye(), Move::getRotation(), 1.0f));
+	mvStack.top() = glm::lookAt(camera::getEye(), camera::getAt(), camera::getUp());
+
 	//draw here
+	// draw a cube for ground plane
+	glBindTexture(GL_TEXTURE_2D, textures[0]);
+	mvStack.push(mvStack.top());
+	mvStack.top() = glm::translate(mvStack.top(), glm::vec3(0.0f, -5.0f, 0.0f));
+	mvStack.top() = glm::scale(mvStack.top(), glm::vec3(100.0f, 0.1f, 100.0f));
+	mvStack.top() = glm::rotate(mvStack.top(), glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	rt3d::setUniformMatrix4fv(shaderProgram, "modelview", glm::value_ptr(mvStack.top()));
+	rt3d::setMaterial(shaderProgram, material0);
+	rt3d::drawIndexedMesh(meshObjects[0], cubeIndexCount, GL_TRIANGLES);
+	mvStack.pop();
 
 	SDL_GL_SwapWindow(window); // swap buffers
 }
--- Honours Project/Honours Project/Game.h
+++ Honours Project/Honours Project/Game.h
@@ -1,10 +1,41 @@
 #pragma once
 #include "Window.h"
 #include "rt3d.h"
+#include "rt3dObjLoader.h"
+#include "Camera.h"
+#include "Move.h"
+#include "loadTexture.h"
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <stack>
+#include <Windows.h>
+#include <mmsystem.h>
+#include <iostream>
+#include <vector>
 
 class Game
 {
 private:
+	//shader variables
+	GLuint shaderProgram;
+	rt3d::lightStruct light0;
+	rt3d::materialStruct material0;
+	GLuint textures[2];
+
+	//stack of models
+	std::stack<glm::mat4> mvStack;
+	vector<GLfloat> verts;
+	vector<GLfloat> norms;
+	vector<GLfloat> tex_coords;
+	vector<GLuint> indices;
+	GLuint meshIndexCount[1];
+	GLuint md2VertCount = 0;
+	GLuint meshObjects[3];
+	GLuint cubeIndexCount = 36;
+
+	//camera
 
 public:
 	void init();
