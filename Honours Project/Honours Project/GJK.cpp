35697b81f8e3fe0bf43def8536aee1a2bfdd64ff
CraigMcCorrisken <craigmccorrisken@gmail.com>
2020-02-10 22:32:09 +0000

    0.3.0 - Working GJK, using a some code which isn't mine, all borrowed code is marked by comments and will be updated accordingly
    Signed-off-by: CraigMcCorrisken <craigmccorrisken@gmail.com>

--- Honours Project/Honours Project/GJK.cpp
+++ Honours Project/Honours Project/GJK.cpp
@@ -39,112 +39,107 @@ bool GJK::performGJK(/*vector<glm::vec3> hull1, vector<glm::vec3> hull2*/)
     // convex hull is passed back the game as a set of points stored in a vector
     // points are written to console window for testing purposes
 
-    //CONVEX HULL CALCULATOR
-    int size = points.size();
-    vector<glm::vec3> hull1;
-    vector<glm::vec3> hull2;
+    ////CONVEX HULL CALCULATOR
+    //int size = points.size();
+    //vector<glm::vec3> hull1;
+    //vector<glm::vec3> hull2;
 
-    if (size >= 4) {	//make sure there are enough points for a 3D shape
-        hull1 = cHull->createHull(points);
-        hull2 = cHull->createHull(points2);
-    }
-    else
-        cout << "not enought vertices for 3D shape, minimum required: 4.\nCurrent vertex count"
-        << size << endl;
+    //if (size >= 4) {	//make sure there are enough points for a 3D shape
+    //    hull1 = cHull->createHull(points);
+    //    hull2 = cHull->createHull(points2);
+    //}
+    //else
+    //    cout << "not enought vertices for 3D shape, minimum required: 4.\nCurrent vertex count"
+    //    << size << endl;
 
-    cout << "Points on first hull: " << endl;
-    for (int i = 0; i <= hull1.size() - 1; i++)
-        cout << "(" << hull1[i].x << ", " << hull1[i].y << ", " << hull1[i].z << ")\n";
+    //cout << "Points on first hull: " << endl;
+    //for (int i = 0; i <= hull1.size() - 1; i++)
+    //    cout << "(" << hull1[i].x << ", " << hull1[i].y << ", " << hull1[i].z << ")\n";
 
-    cout << "Points on second hull: " << endl;
-    for (int i = 0; i <= hull2.size() - 1; i++)
-        cout << "(" << hull2[i].x << ", " << hull2[i].y << ", " << hull2[i].z << ")\n";
-
-
-
-
-    
-
-
-    direction = glm::vec3(0, 1, 0);
-
-    unsigned int furthestIndex = support->furthestPoint(direction, hull1);
-    cout << "\n\n" << endl;
-    cout << "furthest point: " << furthestIndex << endl;
-
-    glm::vec3 furthestVertice = points[furthestIndex];
-    cout << "x: " << furthestVertice.x << endl;
-    cout << "y: " << furthestVertice.y << endl;
-    cout << "z: " << furthestVertice.z << endl;
-
-    //direction  two point clouds  
-    glm::vec3 furthestPoint = support->support(direction, hull1, hull2);
-    cout << "\n\n" << endl;
-    cout << "x: " << furthestPoint.x << endl;
-    cout << "y: " << furthestPoint.y << endl;
-    cout << "z: " << furthestPoint.z << endl;
+    //cout << "Points on second hull: " << endl;
+    //for (int i = 0; i <= hull2.size() - 1; i++)
+    //    cout << "(" << hull2[i].x << ", " << hull2[i].y << ", " << hull2[i].z << ")\n";
 
 
 
 
 
 
-    //TESTING THE GJK
-    cout << "TESTING THE GJK" << endl;
 
-    unsigned int index = 0; // index of current vertex of the simplex
-    glm::vec3 a, b, c, ao, ab, ac, abperp, acperp, simplex[3];
+    //direction = glm::vec3(0, 1, 0);
 
-    // if initial direction is zero then set to any arbitrary axis
-    if ((direction.x == 0) && (direction.y == 0))
-        direction.x = 1.f;
+    //unsigned int furthestIndex = support->furthestPoint(direction, hull1);
+    //cout << "\n\n" << endl;
+    //cout << "furthest point: " << furthestIndex << endl;
 
-    // set the first support as initial point of the new simplex
-    a = simplex[0] = support->support(direction, hull1, hull2);
+    //glm::vec3 furthestVertice = points[furthestIndex];
+    //cout << "x: " << furthestVertice.x << endl;
+    //cout << "y: " << furthestVertice.y << endl;
+    //cout << "z: " << furthestVertice.z << endl;
 
-    if (dot(a, direction) <= 0) //no collision
-        return false;
+    ////direction  two point clouds  
+    //glm::vec3 furthestPoint = support->support(direction, hull1, hull2);
+    //cout << "\n\n" << endl;
+    //cout << "x: " << furthestPoint.x << endl;
+    //cout << "y: " << furthestPoint.y << endl;
+    //cout << "z: " << furthestPoint.z << endl;
 
-    direction = -a; //check the opposite direction
 
-    while (1) {
-        iter_count++;
-        a = simplex[0] = support->support(direction, hull1, hull2);
-        if (dot(a, direction) <= 0) // no collision
-            return false;
 
-        ao = -a; // distance between A-O
 
-        // building up the simplex
-        if (index < 3) {
-            b = simplex[0];
-            ab = b - a; // from point A to B
-            direction = tripleProduct(ab, ao, ab); // normal to AB towards Origin
-            if ((direction.x * direction.x + direction.y * direction.y + direction.z * direction.z) == 0)
-                direction.x = -direction.x;
-            continue; // skip to next iteration
-        }
 
-        b = simplex[1];
-        c = simplex[0];
-        ab = b - a; // from point A to B
-        ac = c - a; // from point A to C
 
-        acperp = tripleProduct(ab, ac, ac);
+    ////TESTING THE GJK
+    //cout << "TESTING THE GJK" << endl;
 
-        if (dot(acperp, ao) >= 0)
-            direction = acperp; // new direction is normal to AC towards Origin
-        else {
-            abperp = tripleProduct(ac, ab, ab);
-            if (dot(abperp, ao) < 0)
-                return true; // collision
-            simplex[0] = simplex[1]; // swap first element (point C)
-            direction = abperp; // new direction is normal to AB towards Origin
-        }
-        simplex[1] = simplex[2]; // swap element in the middle (point B)
-        --index;
-    }
-    return false;
+    //unsigned int index = 0; // index of current vertex of the simplex
+    //glm::vec3 a, b, c, ao, ab, ac, abperp, acperp, simplex[3];
+
+    //// if initial direction is zero then set to any arbitrary axis
+    //if ((direction.x == 0) && (direction.y == 0))
+    //    direction.x = 1.f;
+
+    //// set the first support as initial point of the new simplex
+    //a = simplex[0] = support->support(direction, hull1, hull2);
+
+    //if (dot(a, direction) <= 0) //no collision
+    //    return false;
+
+    //direction = -a; //check the opposite direction
+
+	// TEST CODE ONLY!!
+	glm::vec3 dir = glm::vec3(1, 1, 1);
+
+	c = support->support(dir, points, points2);
+
+	dir = -c;//negative direction
+
+	b = support->support(dir, points, points2);
+
+	if (dot(b, dir) < 0)
+	{
+		return false;
+	}
+	dir = doubleCross(c - b, -b);
+
+	nrPointsSimplex = 2; //begin with 2 points in simplex
+
+	int steps = 0;//avoid infinite loop
+	while (steps < 50)
+	{
+		a = support->support(dir, points, points2);
+		if (dot(a,dir) < 0)
+			return false;
+		else
+		{
+			if (ContainsOrigin(dir))
+			{
+				return true;
+			}
+		}
+		steps++;
+	}
+	return false;
 }
 
 
