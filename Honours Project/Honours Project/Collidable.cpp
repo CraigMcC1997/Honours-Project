eb78510b64bae6193de30803c1ecae5582d27ea8
CraigMcCorrisken <craigmccorrisken@gmail.com>
2020-02-25 18:23:47 +0000

    1.1.5 - Beginning implementation of the QUICKHULL for collidable class
    Signed-off-by: CraigMcCorrisken <craigmccorrisken@gmail.com>

--- Honours Project/Honours Project/Collidable.cpp
+++ Honours Project/Honours Project/Collidable.cpp
@@ -5,7 +5,10 @@
 //instead of just storing the points as is
 void Collidable::setConvexHull(vector<glm::vec3> points)
 {
-	convexHull = points;
+	auto hull = qh.getConvexHull(&points[0].x, points.size(), true, false);
+	//auto hull = qh.getConvexHull(points, true, false);
+	auto indexBuffer = hull.getIndexBuffer();
+	auto vertexBuffer = hull.getVertexBuffer();
 }
 
 vector<glm::vec3> Collidable::getConvexHull()
--- Honours Project/Honours Project/Collidable.h
+++ Honours Project/Honours Project/Collidable.h
@@ -1,13 +1,16 @@
 #pragma once
 #include <glm/glm.hpp>
 #include <vector>
+#include "Quickhull/QuickHull.hpp"
 
 using namespace std;
+using namespace quickhull;
 
 class Collidable
 {
 private:
 	vector<glm::vec3> convexHull;
+	QuickHull<float> qh; // Could be double as well
 public:
 	void setConvexHull(vector<glm::vec3> points);
 	vector<glm::vec3> getConvexHull();
